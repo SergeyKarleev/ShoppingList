55a99560ac5e4e878833b2abe1ded550595f8444
SergeyKarleev <Shaman2889@gmail.com>
2014-09-04 17:11:12 +0400

    Ещё одно сохранение

--- ProjectCommerce/src/com/example/projectcommerce/classes/MyAbstractMigrator.java
+++ ProjectCommerce/src/com/example/projectcommerce/classes/MyAbstractMigrator.java
@@ -1,5 +1,13 @@
 package com.example.projectcommerce.classes;
 
 public abstract class MyAbstractMigrator {
-
+	
+	/**Метод импорта базы на SD карту
+	 * 
+	 */
+	private void Import(){
+		
+	}
+	
+	q
 }
